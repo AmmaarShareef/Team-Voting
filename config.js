bfbdd0ea6ce4546378e5d2dfe68422074c817ab3
AmmaarShareef <Ammhood1602@gmail.com>
2021-12-03 14:25:51 +0400

    Added the config

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAMwm0sD22day1ltPuZ9NDWXi7yYlu5s0E",
+    authDomain: "voting-app-d996a.firebaseapp.com",
+    databaseURL: "https://voting-app-d996a-default-rtdb.firebaseio.com",
+    projectId: "voting-app-d996a",
+    storageBucket: "voting-app-d996a.appspot.com",
+    messagingSenderId: "489657984400",
+    appId: "1:489657984400:web:0adcaee5152f2ce0ad7f15"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
